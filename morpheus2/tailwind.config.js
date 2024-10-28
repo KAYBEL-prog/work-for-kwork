1b631a48e8c190b355f28ad515d08d89633bc27b
kleimeri <klimopjesys@gmail.com>
2024-10-28 12:22:17 +0300

    awdawdawd

--- morpheus2/tailwind.config.js
+++ morpheus2/tailwind.config.js
@@ -4,8 +4,9 @@ module.exports = {
   theme: {
     extend: {
       screens: {
+        'max-xxl2': { 'max': '1530px' },
+        'max-xxl': { 'max': '1280px' },
         'max-lg': { 'max': '1024px' },
-        'max-xxl': { 'max': '1281px' },
         'max-xl': { 'max': '1281px' },
         'max-xl2': { 'max': '950px' },
         'max-md': { 'max': '772px' },
