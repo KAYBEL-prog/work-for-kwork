96a638070e7c0d4f0dd31e405e7c389f2071a166
kleimeri <klimopjesys@gmail.com>
2024-12-08 14:59:23 +0300

    awd

---
+++ adara/send-mail.php
@@ -0,0 +1,29 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Получаем данные из формы
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
+
+    // Email, куда будет отправлено сообщение
+    $to = "klimopjesys@gmail.com";
+    $subject = "Запрос на консультацию";
+    $message = "
+        Имя: $name\n
+        Почта: $email\n
+        Телефон: $phone\n
+    ";
+    $headers = "From: webmaster@example.com\r\n" .
+               "Reply-To: $email\r\n" .
+               "Content-Type: text/plain; charset=UTF-8";
+
+    // Отправляем письмо
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Сообщение отправлено успешно.";
+    } else {
+        echo "Ошибка отправки сообщения.";
+    }
+} else {
+    echo "Некорректный метод отправки данных.";
+}
+?>
