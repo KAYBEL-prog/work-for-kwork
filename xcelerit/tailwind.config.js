3884818a5cf084fa6df8f6616b914c296d083344
klim <klimopjesys@gmail.com>
2025-03-10 10:07:30 +0300

    awd

---
+++ xcelerit/tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Manrope", "sans-serif"],
+      },
+      fontSize: {
+        tiny: "40px",
+        h1: "72px",
+        h2: "52px",
+        h3: "46px",
+        h4: "32px",
+      },
+      colors: {
+        'brand': "#FFC800",
+        grey: {
+          1: "#202020",
+          2: "#494A4E",
+          3: "#E6E3DE",
+        },
+      },
+      backgroundImage: {
+        'hero-block': "url('../assets/img/pages-home/bg-hero-block.png')",
+        'bg-items': "url('../assets/img/pages-home/bg-items.png')",
+        'why-xcelerit-inner': "url('../assets/img/pages-home/bg-hero-block__why-xcelerit.png')",
+        'bgbgbg': "url('../assets/img/bgbg.png')",
+      },
+    },
+  },
+  plugins: [],
+};
