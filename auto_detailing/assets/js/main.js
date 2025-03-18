8397dba44f1e039dd5317887ce78b1c837e5ac8c
klim <klimopjesys@gmail.com>
2025-03-19 00:37:08 +0300

    update "auto_detailing"

--- auto_detailing/assets/js/main.js
+++ auto_detailing/assets/js/main.js
@@ -6,3 +6,9 @@ window.addEventListener('scroll', function () {
     header.style.backgroundColor = 'transparent';
   }
 });
+
+document.querySelector('#burgerMenuBtn').addEventListener('click', () => {
+  document.querySelector('.burger-menu__line1').classList.toggle('active');
+  document.querySelector('.burger-menu__line2').classList.toggle('d-none');
+  document.querySelector('.burger-menu__line3').classList.toggle('active');
+});
