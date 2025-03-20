b055968f3e3a980a8f26f5999778c1045a15382c
klim <klimopjesys@gmail.com>
2025-03-20 15:02:52 +0300

    update "auto_detailing" correction

--- auto_detailing/assets/js/main.js
+++ auto_detailing/assets/js/main.js
@@ -8,7 +8,15 @@ window.addEventListener('scroll', function () {
 });
 
 document.querySelector('#burgerMenuBtn').addEventListener('click', () => {
-  document.querySelector('.burger-menu__line1').classList.toggle('active');
-  document.querySelector('.burger-menu__line2').classList.toggle('d-none');
-  document.querySelector('.burger-menu__line3').classList.toggle('active');
+  if (document.querySelector('.burger__menu-icon-1').classList.contains('d-block')) {
+    document.querySelector('.burger__menu-icon-1').classList.add('d-none');
+    document.querySelector('.burger__menu-icon-1').classList.remove('d-block');
+    document.querySelector('.burger__menu-icon-2').classList.remove('d-none');
+    document.querySelector('.burger__menu-icon-2').classList.add('d-block');
+  } else {
+    document.querySelector('.burger__menu-icon-2').classList.add('d-none');
+    document.querySelector('.burger__menu-icon-2').classList.remove('d-block');
+    document.querySelector('.burger__menu-icon-1').classList.remove('d-none');
+    document.querySelector('.burger__menu-icon-1').classList.add('d-block');
+  }
 });
