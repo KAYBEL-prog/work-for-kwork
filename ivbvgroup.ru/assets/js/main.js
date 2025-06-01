acf9d9c49597fdc3897c4ee6a2eda988d7cecfe5
klim <klimopjesys@gmail.com>
2025-06-01 15:42:50 +0300

    awd

---
+++ ivbvgroup.ru/assets/js/main.js
@@ -0,0 +1,41 @@
+// burger-menu
+document.querySelector('#burger-menu').addEventListener('click', () => {
+  if (document.querySelector('.burger-menu-container').style.display == 'flex') {
+    document.querySelector('.burger-menu-container').style.display = 'none';
+  } else {
+    document.querySelector('.burger-menu-container').style.display = 'flex';
+  }
+  console.log('awda');
+});
+
+
+
+// form
+document.querySelector('#form0-btn').addEventListener('click', () => {
+  document.querySelector('.inner__form-po__block-form0').style.display = 'none';
+  document.querySelector('.inner__form-po__block-form1').style.display = 'block';
+});
+
+document.querySelector('#btn_back1').addEventListener('click', () => {
+  document.querySelector('.inner__form-po__block-form1').style.display = 'none';
+  document.querySelector('.inner__form-po__block-form0').style.display = 'block';
+});
+document.querySelector('#btn_next1').addEventListener('click', () => {
+  document.querySelector('.inner__form-po__block-form1').style.display = 'none';
+  document.querySelector('.inner__form-po__block-form2').style.display = 'block';
+});
+
+document.querySelector('#btn_back2').addEventListener('click', () => {
+  document.querySelector('.inner__form-po__block-form1').style.display = 'block';
+  document.querySelector('.inner__form-po__block-form2').style.display = 'none';
+});
+document.querySelector('#btn_next2').addEventListener('click', () => {
+  document.querySelector('.inner__form-po__block-form2').style.display = 'none';
+  document.querySelector('.inner__form-po__block-form3').style.display = 'block';
+});
+
+document.querySelector('#btn_back3').addEventListener('click', () => {
+  document.querySelector('.inner__form-po__block-form3').style.display = 'none';
+  document.querySelector('.inner__form-po__block-form2').style.display = 'block';
+});
+
