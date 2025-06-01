a9612fe2f60e8f570168773b84219332e48200f7
klim <klimopjesys@gmail.com>
2025-06-01 18:03:44 +0300

    awd

--- ivbvgroup.ru/assets/js/main.js
+++ ivbvgroup.ru/assets/js/main.js
@@ -2,8 +2,11 @@
 document.querySelector('#burger-menu').addEventListener('click', () => {
   if (document.querySelector('.burger-menu-container').style.display == 'flex') {
     document.querySelector('.burger-menu-container').style.display = 'none';
+    document.querySelector('body').style.overflowY = "visible";
+
   } else {
     document.querySelector('.burger-menu-container').style.display = 'flex';
+    document.querySelector('body').style.overflowY = "hidden";
   }
   console.log('awda');
 });
@@ -11,11 +14,10 @@ document.querySelector('#burger-menu').addEventListener('click', () => {
 
 
 // form
-document.querySelector('#form0-btn').addEventListener('click', () => {
+document.querySelector('#btn_next11').addEventListener('click', () => {
   document.querySelector('.inner__form-po__block-form0').style.display = 'none';
   document.querySelector('.inner__form-po__block-form1').style.display = 'block';
 });
-
 document.querySelector('#btn_back1').addEventListener('click', () => {
   document.querySelector('.inner__form-po__block-form1').style.display = 'none';
   document.querySelector('.inner__form-po__block-form0').style.display = 'block';
@@ -24,6 +26,7 @@ document.querySelector('#btn_next1').addEventListener('click', () => {
   document.querySelector('.inner__form-po__block-form1').style.display = 'none';
   document.querySelector('.inner__form-po__block-form2').style.display = 'block';
 });
+
 
 document.querySelector('#btn_back2').addEventListener('click', () => {
   document.querySelector('.inner__form-po__block-form1').style.display = 'block';
